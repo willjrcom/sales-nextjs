fc8e0e7c23d187221661e6f5c2e8ab6eac71d615
willjrcom <williamjunior67@gmail.com>
2024-05-24 23:17:26 -0300

    fix: useRouter removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
 'use client';
 
-import { useRouter } from 'next/router';
 import './globals.css';
 import Menu from './menu';
 
 const Home= () => {
-  const router = useRouter();
-
   return (
     <div className="flex">
       <Menu  />
