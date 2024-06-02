8efa6686e10aac678baf0b6cf5216f19b1cf486a
willjrcom <williamjunior67@gmail.com>
2024-06-02 10:48:14 -0300

    feat: refresh added to employee and client

--- src/app/pages/client/page.tsx
+++ src/app/pages/client/page.tsx
@@ -10,11 +10,14 @@ import ClientColumns from "@/app/entities/client/table-columns";
 import { useEffect, useState } from "react";
 import { Client } from "@/app/entities/client/client";
 import GetClients from "@/app/api/client/route";
+import Refresh, { FormatRefreshTime } from "@/app/components/crud/refresh";
 
 const PageClient = () => {
     const [clients, setClients] = useState<Client[]>([])
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState<string | null>(null);
+    const formattedTime = FormatRefreshTime(new Date())
+    const [lastUpdate, setLastUpdate] = useState(formattedTime);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -36,6 +39,7 @@ const PageClient = () => {
             <CrudLayout title="Clientes"
                 filterButtonChildren={<ButtonFilter name="cliente" />}
                 plusButtonChildren={<ButtonPlus name="cliente" href="/client/new"><CreateClientForm /></ButtonPlus>}
+                refreshButton={<Refresh lastUpdate={lastUpdate} setItems={setClients} getItems={GetClients} setLastUpdate={setLastUpdate} />}
                 tableChildren={<CrudTable columns={ClientColumns()} data={clients}></CrudTable>} />
         </Menu>
     );
--- src/app/pages/employee/page.tsx
+++ src/app/pages/employee/page.tsx
@@ -10,11 +10,14 @@ import EmployeeColumns from "@/app/entities/employee/table-columns";
 import GetEmployees from "@/app/api/employee/route";
 import { Employee } from "@/app/entities/employee/employee";
 import { useEffect, useState } from "react";
+import Refresh, { FormatRefreshTime } from "@/app/components/crud/refresh";
 
 const PageEmployee = () => {
     const [employees, setEmployees] = useState<Employee[]>([])
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState<string | null>(null);
+    const formattedTime = FormatRefreshTime(new Date())
+    const [lastUpdate, setLastUpdate] = useState(formattedTime);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -35,6 +38,7 @@ const PageEmployee = () => {
             <CrudLayout title="Funcionários"
                 filterButtonChildren={<ButtonFilter name="funcionario" />}
                 plusButtonChildren={<ButtonPlus name="funcionario" href="/employee/new"><CreateEmployeeForm /></ButtonPlus>}
+                refreshButton={<Refresh lastUpdate={lastUpdate} setItems={setEmployees} getItems={GetEmployees} setLastUpdate={setLastUpdate} />}
                 tableChildren={<CrudTable columns={EmployeeColumns()} data={employees}></CrudTable>} />
         </Menu>
     );
