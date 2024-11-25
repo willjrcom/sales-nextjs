d00db91e95480e1bfb678b15959198d39a1a0d28
willjrcom <williamjunior67@gmail.com>
2024-11-25 13:40:20 -0300

    feat: error and loading added to layout

---
+++ src/app/pages/error.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function PageError() {
+    return (
+        <div>
+            <h1>Falha ao carregar página</h1>
+        </div>
+    );
+}
--- src/app/pages/layout.tsx
+++ src/app/pages/layout.tsx
@@ -1,6 +1,10 @@
 'use client';
 
+import { ErrorBoundary } from "next/dist/client/components/error-boundary";
 import Menu from "../components/menu/layout";
+import PageError from "./error";
+import { Suspense } from "react";
+import Loading from "./loading";
 
 export default function DashboardLayout({
     children,
@@ -8,8 +12,12 @@ export default function DashboardLayout({
     children: React.ReactNode;
 }) {
     return (
+        <ErrorBoundary errorComponent={PageError}>
+            <Suspense fallback={<Loading />}>
         <Menu>
             {children}
         </Menu>
+        </Suspense>
+        </ErrorBoundary>
     );
 }
