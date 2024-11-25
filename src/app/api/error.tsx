13fc15fdfec5e4e42545c4612906a7465e1ec03b
willjrcom <williamjunior67@gmail.com>
2024-11-24 22:16:58 -0300

    feat: error added to response request

---
+++ src/app/api/error.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default class RequestError {
+    message: string = "";
+    status: number = 0;
+    path: string = "";
+
+    constructor() {};
+}
--- src/app/api/request.tsx
+++ src/app/api/request.tsx
@@ -1,5 +1,5 @@
 import { Session } from "next-auth"
-import { useSession } from "next-auth/react"
+import RequestError from "./error"
 
 interface RequestApiProps<T> {
     path: string
@@ -21,7 +21,7 @@ const AddIdToken = async (session: Session) => {
     if (session.user.idToken === undefined) {
         throw new Error("id token not found in session");
     }
-    
+
     return { "id-token": session.user.idToken }
 }
 
@@ -33,32 +33,39 @@ const AddAccessToken = async (session: Session) => {
     return { "access-token": session.user.id }
 }
 
-const RequestApi = async <T,TR>({path, body, method, headers }: RequestApiProps<T>): Promise<Response<TR>> => {
+const RequestApi = async <T, TR>({ path, body, method, headers }: RequestApiProps<T>): Promise<Response<TR>> => {
     if (!path.startsWith("/")) {
         throw new Error(`path: ${path} must start with /`);
     }
 
     const fullPath = `${process.env.NEXT_PUBLIC_API_BASE_URL}${path}`;
+
     const res = await fetch(fullPath, {
-        method: method,
-        body: JSON.stringify(body),
+        method,
+        body: body ? JSON.stringify(body) : undefined,
         headers: {
             ...jsonHeaders,
-            ...headers
+            ...headers,
         },
     });
-    
 
-    if (!res.ok) {
-        throw new Error("Failed to " + method + ", path: " + fullPath);
+    if (!res.ok && res.status === 500) {
+        const body = await res.json();
+        const error = new RequestError();
+        error.message = body.message || "Erro desconhecido";
+
+        if (error.message == "sql: no rows in result set") {
+            error.message = "Não encontrado";
+        }
+
+        error.status = res.status;
+        error.path = fullPath;
+        throw error;
     }
 
-    if (res.status > 299) {
-        throw new Error("Server error " + method + ", path: " + fullPath + ", status: " + res.status, { cause: res });
-    }
-
-    return await res.json();
+    const parsedBody = (await res.json()) as TR;
+    return {...parsedBody} as Response<TR>;
 };
 
 export default RequestApi
-export { AddIdToken, AddAccessToken}
+export { AddIdToken, AddAccessToken }
