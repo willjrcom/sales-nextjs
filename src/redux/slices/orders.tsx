e1b5415d3cfda4efa68c7717c65a387bda4c1b23
willjrcom <williamjunior67@gmail.com>
2024-12-13 23:16:33 -0300

    feat: orders slice added

--- src/redux/slices/orders.tsx
+++ src/redux/slices/orders.tsx
@@ -1,10 +1,78 @@
-// redux/slices/counterSlice.ts
+import { GenericsProps, GenericState } from './generics';
+import { createSlice, createAsyncThunk, createEntityAdapter } from '@reduxjs/toolkit';
+import { Session } from 'next-auth';
+import { FormatRefreshTime } from '@/app/components/crud/refresh';
+import RequestError from '@/app/api/error';
 import Order from '@/app/entities/order/order';
-import createGenericSlice from './generics';
-import GetOrders from '@/app/api/order/route';
 
-const ordersSlice = createGenericSlice<Order>({ name: 'orders', getItems: GetOrders });
+// Configuração genérica do slice
+const createOrdersSlice = ({ name, getItems }: GenericsProps<Order>) => {
+    const adapter = createEntityAdapter<Order, string>({
+        // Assume IDs are stored in a field other than `t.id`
+        selectId: (t: Order) => t.id,
+        // Keep the "all IDs" array sorted based on t order_number
+        sortComparer: (a, b) => a.order_number - b.order_number,
+    })
 
-export const { addItem: addOrder, removeItem: removeOrder, updateItem: updateOrder } = ordersSlice.actions;
-export const { fetchItems: fetchOrders } = ordersSlice;
-export default ordersSlice.reducer;
+    // Combina o estado inicial do adapter com estados adicionais
+    const initialState = adapter.getInitialState<GenericState>({
+        loading: false,
+        error: null,
+        lastUpdate: FormatRefreshTime(new Date()),
+    });
+
+    // Criar o thunk assíncrono para buscar dados
+    const fetchOrders = createAsyncThunk(`${name}/fetch`, async (session: Session, { rejectWithValue }) => {
+        try {
+            const items = await getItems(session);
+            return items;
+        } catch (error) {
+            return rejectWithValue(error);
+        }
+    });
+
+    // Criar o slice
+    const slice = createSlice({
+        name,
+        initialState,
+        reducers: {
+            addOrder: (state, action) => {
+                adapter.addOne(state, action.payload);
+            }, // Adiciona um item
+            removeOrder: (state, action) => {
+                adapter.updateOne(state, action.payload);
+            }, // Atualiza um item
+            updateOrder: (state, action) => {
+                adapter.removeOne(state, action.payload);
+            }, // Remove um item
+        },
+        extraReducers: (builder) => {
+            builder
+                .addCase(fetchOrders.pending, (state) => {
+                    state.loading = true;
+                })
+                .addCase(fetchOrders.fulfilled, (state, action) => {
+                    state.loading = false;
+                    adapter.setAll(state, action.payload); // Substitui todos os itens
+                    state.lastUpdate = FormatRefreshTime(new Date());
+                })
+                .addCase(fetchOrders.rejected, (state, action) => {
+                    state.loading = false;
+                    state.error = action.payload as RequestError;
+                });
+        },
+    });
+
+    // Retornar o slice configurado
+    return {
+        reducer: slice.reducer,
+        actions: slice.actions,
+        fetchItems: fetchOrders,
+        adapterSelectors: adapter.getSelectors((state: any) => state[name]), // Seletores do adapter
+    };
+};
+
+const orderSlice = createOrdersSlice({ name: 'orders', getItems: async () => [] })
+export const { addOrder, removeOrder, updateOrder } = orderSlice.actions;
+export const { fetchItems: fetchOrders, adapterSelectors } = orderSlice;
+export default orderSlice.reducer;
