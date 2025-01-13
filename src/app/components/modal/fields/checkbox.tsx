75842fa355c838239923fbda7dea41ed41ca8318
willjrcom <williamjunior67@gmail.com>
2025-01-13 10:54:38 -0300

    feat: checkbox improved to toggle

--- src/app/components/modal/fields/checkbox.tsx
+++ src/app/components/modal/fields/checkbox.tsx
@@ -1,31 +1,58 @@
-import { Dispatch, SetStateAction } from "react"
+import { Dispatch, SetStateAction } from "react";
 
 interface CheckboxFieldProps {
-    friendlyName: string
-    name: string
-    disabled?: boolean
-    value?: boolean
-    setValue: Dispatch<SetStateAction<boolean>>
+    friendlyName: string;
+    name: string;
+    disabled?: boolean;
+    value?: boolean;
+    setValue: Dispatch<SetStateAction<boolean>>;
     optional?: boolean;
 }
 
-const CheckboxField = ({ friendlyName, name, disabled, value, setValue, optional}: CheckboxFieldProps) => {
+const CheckboxField = ({
+    friendlyName,
+    name,
+    disabled,
+    value = false,
+    setValue,
+    optional,
+}: CheckboxFieldProps) => {
     return (
         <div className="mb-4">
             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor={name}>
                 {friendlyName} {!optional && <span className="text-red-500">*</span>}
             </label>
 
-            <input
-            className="cursor-pointer"
-                id={name}
-                type="checkbox"
-                disabled={disabled}
-                checked={value} // Aqui você usa `checked` para indicar se o checkbox está marcado ou não
-                onChange={e => setValue(e.target.checked)} // Usa `e.target.checked` que é um booleano
-            />
+            {/* Toggle Switch */}
+            <div
+                className={`relative inline-block w-12 h-6 ${
+                    disabled ? "opacity-50 cursor-not-allowed" : "cursor-pointer"
+                }`}
+                onClick={() => !disabled && setValue(!value)} // Altera o estado ao clicar
+            >
+                <input
+                    id={name}
+                    type="checkbox"
+                    className="opacity-0 w-0 h-0"
+                    checked={value}
+                    disabled={disabled}
+                    onChange={(e) => setValue(e.target.checked)}
+                />
+                {/* Fundo do toggle */}
+                <span
+                    className={`absolute top-0 left-0 right-0 bottom-0 rounded-full transition-all duration-300 ${
+                        value ? "bg-blue-500" : "bg-gray-300"
+                    }`}
+                ></span>
+                {/* Botão do toggle */}
+                <span
+                    className={`absolute top-1 left-1 w-4 h-4 bg-white rounded-full transition-transform duration-300 ${
+                        value ? "transform translate-x-6" : ""
+                    }`}
+                ></span>
+            </div>
         </div>
     );
 };
 
-export default CheckboxField
+export default CheckboxField;
