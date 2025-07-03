a9e7d8ef5ced083accfea62a7ce53a07a2be194b
willjrcom <williamjunior67@gmail.com>
2025-07-03 13:10:16 -0300

    feat: metadata added

--- src/app/pages/layout.tsx
+++ src/app/pages/layout.tsx
@@ -1,10 +1,14 @@
-'use client';
-
 import { ErrorBoundary } from "next/dist/client/components/error-boundary";
 import Menu from "../components/menu/layout";
 import PageError from "./error";
 import { Suspense } from "react";
 import Loading from "./loading";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "GFood - Painel",
+    description: "Sistema de vendas para estabelecimentos",
+};
 
 export default function DashboardLayout({
     children,
