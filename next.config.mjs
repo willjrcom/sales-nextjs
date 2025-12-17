b0d25264a16306d1c18fcbf984b885525ef52a2f
willjrcom <williamjunior67@gmail.com>
2025-12-17 08:07:12 -0300

    feat: s3 changes

--- next.config.mjs
+++ next.config.mjs
@@ -2,15 +2,23 @@
 const nextConfig = {
     crossOrigin: 'anonymous',
     distDir: 'build',
+    output: 'standalone',
     images: {
         minimumCacheTTL: 60 * 60 * 24 * 30, // 30 days to avoid frequent S3 fetches
         remotePatterns: [
             {
                 protocol: 'https',
-                hostname: '*.amazonaws.com',
+                hostname: 'gazaltechgfood.s3.us-east-2.amazonaws.com',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: '*.s3.*.amazonaws.com',
                 pathname: '/**',
             },
         ],
+        unoptimized: false, // Ensure image optimization is enabled
+        dangerouslyAllowSVG: true,
     },
 };
 
