65e8d4a707a52177890794f598b88fe49c136417
willjrcom <williamjunior67@gmail.com>
2026-01-29 14:48:11 -0300

    feat: icon

---
+++ src/app/icon.tsx
@@ -0,0 +1,44 @@
+import { ImageResponse } from 'next/og'
+
+// Route segment config
+export const runtime = 'edge'
+
+// Image metadata
+export const size = {
+    width: 32,
+    height: 32,
+}
+export const contentType = 'image/png'
+
+// Image generation
+export default function Icon() {
+    return new ImageResponse(
+        (
+            // ImageResponse JSX element
+            <div
+                style={{
+                    fontSize: 24,
+                    background: 'white',
+                    width: '100%',
+                    height: '100%',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    color: '#ef4444', // Red-500
+                    fontWeight: 900,
+                    borderRadius: 4,
+                    // Adding a border to make it distinct on white tabs
+                    border: '1px solid #ef4444',
+                }}
+            >
+                G
+            </div>
+        ),
+        // ImageResponse options
+        {
+            // For convenience, we can re-use the exported icons size metadata
+            // config to also set the ImageResponse's width and height.
+            ...size,
+        }
+    )
+}
